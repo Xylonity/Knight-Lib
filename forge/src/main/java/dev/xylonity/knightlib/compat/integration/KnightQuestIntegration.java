57d6c421a3a42f057b7e22d8cccc25adb0a75fad
xylonity <loldanielespia@gmail.com>
2024-12-24 19:05:22 +0100

    💛 Added compatibility with knightquest 1.8.0

--- forge/src/main/java/dev/xylonity/knightlib/compat/integration/KnightQuestIntegration.java
+++ forge/src/main/java/dev/xylonity/knightlib/compat/integration/KnightQuestIntegration.java
@@ -1,11 +1,10 @@
 package dev.xylonity.knightlib.compat.integration;
 
+import dev.xylonity.knightquest.common.entity.boss.NethermanEntity;
+import dev.xylonity.knightquest.registry.KnightQuestEntities;
+import dev.xylonity.knightquest.registry.KnightQuestItems;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
-import net.xylonity.knightquest.common.entity.boss.NethermanEntity;
-import net.xylonity.knightquest.config.values.KQConfigValues;
-import net.xylonity.knightquest.registry.KnightQuestEntities;
-import net.xylonity.knightquest.registry.KnightQuestItems;
 
 /**
  * Bridge to avoid crashes when specific mods are not present and some internal classes is called.
@@ -16,20 +15,16 @@ public class KnightQuestIntegration {
         return KnightQuestItems.RADIANT_ESSENCE.get();
     }
 
+    public static Item getChaoticEssence() {
+        return KnightQuestItems.CHAOTIC_ESSENCE.get();
+    }
+
     public static Item getEmptyGoblet() {
         return KnightQuestItems.EMPTY_GOBLET.get();
     }
 
     public static Item getFilledGoblet() {
         return KnightQuestItems.FILLED_GOBLET.get();
-    }
-
-    public static boolean configCanSummonNetherman() {
-        return KQConfigValues.CAN_SUMMON_NETHERMAN;
-    }
-
-    public static boolean configSpawnLightningOnSpawn() {
-        return KQConfigValues.SPAWN_LIGHTNING_ON_SPAWN;
     }
 
     public static EntityType<NethermanEntity> nethermanEntity() {
