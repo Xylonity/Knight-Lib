39b40e694f217fd21f2f46b533fb8ff3a42b7f2c
xylonity <42015327+Xylonity@users.noreply.github.com>
2025-06-16 21:32:26 +0200

    🍳 Updated starset particle

--- forge/src/main/java/dev/xylonity/knightlib/common/event/KnightLibClientEvents.java
+++ forge/src/main/java/dev/xylonity/knightlib/common/event/KnightLibClientEvents.java
@@ -3,11 +3,14 @@ package dev.xylonity.knightlib.common.event;
 import dev.xylonity.knightlib.KnightLib;
 import dev.xylonity.knightlib.client.blockentity.renderer.GreatChaliceRenderer;
 import dev.xylonity.knightlib.client.projectile.renderer.GreatChaliceStarsetRingRenderer;
+import dev.xylonity.knightlib.common.particle.StarsetParticle;
 import dev.xylonity.knightlib.registry.KnightLibBlockEntities;
 import dev.xylonity.knightlib.registry.KnightLibEntities;
+import dev.xylonity.knightlib.registry.KnightLibParticles;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -20,6 +23,11 @@ public class KnightLibClientEvents {
         EntityRenderers.register(KnightLibEntities.GREAT_CHALICE_STARSET_RING.get(), GreatChaliceStarsetRingRenderer::new);
 
         BlockEntityRenderers.register(KnightLibBlockEntities.GREAT_CHALICE.get(), GreatChaliceRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void registerParticleFactories(final RegisterParticleProvidersEvent event) {
+        event.registerSpriteSet(KnightLibParticles.STARSET.get(), StarsetParticle.Provider::new);
     }
 
 }
