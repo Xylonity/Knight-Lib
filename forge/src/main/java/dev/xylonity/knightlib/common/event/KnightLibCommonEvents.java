5444ab785d733cbc4f171fc27b33e5816ae34452
xylonity <42015327+Xylonity@users.noreply.github.com>
2025-06-17 03:25:56 +0200

    ⭕ Added TickScheduler

---
+++ forge/src/main/java/dev/xylonity/knightlib/common/event/KnightLibCommonEvents.java
@@ -0,0 +1,53 @@
+package dev.xylonity.knightlib.common.event;
+
+import dev.xylonity.knightlib.KnightLib;
+import dev.xylonity.knightlib.common.api.TickScheduler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = KnightLib.MOD_ID)
+public class KnightLibCommonEvents {
+
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public static void onServerTick(TickEvent.ServerTickEvent event) {
+        if (event.phase != TickEvent.Phase.END) return;
+
+        TickScheduler.clean();
+
+        for (ServerLevel level : event.getServer().getAllLevels()) {
+            TickScheduler.incrementTick(level);
+            TickScheduler.processServerTasks(level);
+            TickScheduler.processCommonTasks(level);
+        }
+
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase != TickEvent.Phase.END) return;
+
+        Minecraft minecraft = Minecraft.getInstance();
+        Level level = minecraft.level;
+        if (level == null) return;
+
+        TickScheduler.clean();
+        TickScheduler.incrementTick(level);
+        TickScheduler.processClientTasks(level);
+        TickScheduler.processCommonTasks(level);
+    }
+
+    @SubscribeEvent
+    public static void onLevelUnload(LevelEvent.Unload event) {
+        if (event.getLevel() instanceof Level level) {
+            TickScheduler.markForClean(level);
+        }
+
+    }
+
+}
