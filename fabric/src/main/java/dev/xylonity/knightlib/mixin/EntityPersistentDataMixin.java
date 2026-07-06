8ee6940766f56d22121e4628295b9a75a2c17b28
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-07-06 16:48:48 +0200

    🌛 Added attachments cache

--- fabric/src/main/java/dev/xylonity/knightlib/mixin/EntityPersistentDataMixin.java
+++ fabric/src/main/java/dev/xylonity/knightlib/mixin/EntityPersistentDataMixin.java
@@ -2,6 +2,7 @@ package dev.xylonity.knightlib.mixin;
 
 import dev.xylonity.knightlib.common.entity.data.internal.KnightLibPersistentDataAccessor;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -10,6 +11,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Mixin(Entity.class)
 public abstract class EntityPersistentDataMixin implements KnightLibPersistentDataAccessor {
 
@@ -17,8 +21,16 @@ public abstract class EntityPersistentDataMixin implements KnightLibPersistentDa
     private CompoundTag knightlib$persistentData = new CompoundTag();
 
     @Unique
+    private final Map<ResourceLocation, Object> knightlib$attachmentCache = new ConcurrentHashMap<>();
+
+    @Unique
     public CompoundTag knightlib$getPersistentData() {
         return knightlib$persistentData;
+    }
+
+    @Unique
+    public Map<ResourceLocation, Object> knightlib$getAttachmentCache() {
+        return knightlib$attachmentCache;
     }
 
     @Inject(method = "saveWithoutId", at = @At("RETURN"))
@@ -33,6 +45,7 @@ public abstract class EntityPersistentDataMixin implements KnightLibPersistentDa
     private void knightlib$loadPersistentData(CompoundTag tag, CallbackInfo ci) {
         if (tag.contains("KnightLibData", CompoundTag.TAG_COMPOUND)) {
             knightlib$persistentData = tag.getCompound("KnightLibData");
+            knightlib$attachmentCache.clear();
         }
 
     }
--- forge/src/main/java/dev/xylonity/knightlib/mixin/EntityPersistentDataMixin.java
+++ forge/src/main/java/dev/xylonity/knightlib/mixin/EntityPersistentDataMixin.java
@@ -2,16 +2,28 @@ package dev.xylonity.knightlib.mixin;
 
 import dev.xylonity.knightlib.common.entity.data.internal.KnightLibPersistentDataAccessor;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Mixin(Entity.class)
 public abstract class EntityPersistentDataMixin implements KnightLibPersistentDataAccessor {
 
     @Unique
+    private final Map<ResourceLocation, Object> knightlib$attachmentCache = new ConcurrentHashMap<>();
+
+    @Unique
     public CompoundTag knightlib$getPersistentData() {
         return ((Entity) (Object) this).getPersistentData();
+    }
+
+    @Unique
+    public Map<ResourceLocation, Object> knightlib$getAttachmentCache() {
+        return knightlib$attachmentCache;
     }
 
 }
